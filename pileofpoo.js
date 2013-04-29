e30e30e91edddfa11e30f44c62d72aa1a96d0e2a
Peter Jaap Blaakmeer <info@blaakmeer.com>
2013-04-29 16:42:59 +0200

    Built in support for non-Firefox browsers

--- pileofpoo.js
+++ pileofpoo.js
@@ -1,5 +1,5 @@
 $(window).ready(function () {
-    if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+    if((navigator.userAgent.toLowerCase().indexOf('firefox') > -1) || (navigator.userAgent.toLowerCase().indexOf('safari') > -1 && navigator.userAgent.toLowerCase().indexOf('chrome') == -1)) {
         var replaceWith = '&#128169;'; 
     } else {
         var replaceWith = '<img src="assets/1f4a9.png" title="this is smelly" />';
